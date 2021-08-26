8d6c4d857f2ce97b94851a27c975269429af687e
Sam Niyazi <24701208+SamNiyazi2@users.noreply.github.com>
2021-08-26 15:48:50 -0500

    M04-07 - Add assembly lifecycle methods. Same assue as class level, initialize works but not cleanup.

---
+++ GameEngine.Tests/Assembly.cs
@@ -0,0 +1,27 @@
+﻿
+// 08/26/2021 03:41 pm - SSN - [20210826-1537] - [001] - M04-07 - Running additional code at the assembly level
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GameEngine.Tests
+{
+    [TestClass]
+    public class Assembly
+    {
+
+        [AssemblyInitialize]
+        public static void AssemblyInit(TestContext testContext)
+        {
+            System.Console.WriteLine("Assembly.AssemblyInit [AssemblyInitialize]:");
+        }
+
+
+        [AssemblyCleanup]
+        public static void AssemblyCleanup()
+        {
+            System.Console.WriteLine("Assembly.AssemblyCleanup [AssemblyCleanup]:");
+        }
+
+
+    }
+}
