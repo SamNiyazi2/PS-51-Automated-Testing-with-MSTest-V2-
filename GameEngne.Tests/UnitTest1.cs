953e7d106f04e14cdb9daab65fa980ac6a91cea1
Sam Niyazi <24701208+SamNiyazi2@users.noreply.github.com>
2021-08-26 08:56:43 -0500

    Add test project.

---
+++ GameEngne.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+// 08/26/2021 08:51 am - SSN - [20210826-0850] - [001] - M02-05 - Creating a new test project
+
+namespace GameEngne.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
