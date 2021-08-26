93b502282225d42dae9f0b3ff0068dc17afe2d22
Sam Niyazi <24701208+SamNiyazi2@users.noreply.github.com>
2021-08-26 12:39:23 -0500

    M03-09 - Asserting thrown exceptions.

---
+++ GameEngne.Tests/EnemyFactoryShould.cs
@@ -0,0 +1,63 @@
+﻿using GameEngine;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Threading.Tasks;
+
+// 08/26/2021 12:21 pm - SSN - [20210826-1220] - [001] - M03-09 - Asserting that the correct exceptions are thrown
+
+namespace GameEngne.Tests
+{
+    [TestClass]
+    public class EnemyFactoryShould
+    {
+
+        [TestMethod]
+        public void NotAllowNullName()
+        {
+
+            // Arrange
+
+            var sut = new EnemyFactory();
+
+            // Act
+
+
+            // Assert
+
+            Assert.ThrowsException<ArgumentNullException>(() => sut.Create(null));
+            Assert.ThrowsExceptionAsync<ArgumentNullException>(async () => await Task.Run(() => sut.Create(null)));
+
+        }
+
+
+        [TestMethod]
+        public void OnlyAllowKingOrQueenBossEnemies()
+        {
+
+            // Arrange
+
+            var sut = new EnemyFactory();
+
+            string enemyName = "Zombie";
+
+            // Act
+
+
+            // Assert
+
+            EnemyCreationException ex = Assert.ThrowsException<EnemyCreationException>(() => sut.Create(enemyName, isBoss: true));
+
+
+            Console.WriteLine("OnlyAllowKingOrQueenBossEnemies");
+            Console.WriteLine("Typeof ex:");
+            Console.WriteLine(ex.GetType().Name);
+
+
+            Assert.AreEqual(enemyName, ex.RequestedEnemyName);
+
+        }
+
+    }
+}
+
+
